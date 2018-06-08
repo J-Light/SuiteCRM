b7f8b1d8103d95f914cf68f987a60a0050aebe3d
Nishit Joseph <nish.joseph@leapaust.com.au>
2018-06-08 13:28:55 +1000

    add the agreement number in "Products and Services" subpanel under accounts module Part 2

--- custom/modules/AOS_Products_Quotes/metadata/subpanels/Account_subpanel_products_services_purchased.php
+++ custom/modules/AOS_Products_Quotes/metadata/subpanels/Account_subpanel_products_services_purchased.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2017-10-13 13:42:35
+// created: 2018-06-05 14:34:59
 $subpanel_layout['list_fields'] = array (
   'name' => 
   array (
@@ -10,13 +10,16 @@ $subpanel_layout['list_fields'] = array (
     'width' => '25%',
     'default' => true,
   ),
-  'invoice_number'=>array(
-			'link' => true,
-			'vname' => 'LBL_ACCOUNTS_PRODUCTS_SERVICES_INVOICE', // Quote
-			'widget_class' => 'SubPanelDetailViewLink',
-			'target_record_key' => 'invoice_id',
-			'target_module'=>'AOS_Invoices', // or 'target_module' => 'AOS_Quotes',
-		),
+  'invoice_number' => 
+  array (
+    'link' => true,
+    'vname' => 'LBL_ACCOUNTS_PRODUCTS_SERVICES_INVOICE',
+    'widget_class' => 'SubPanelDetailViewLink',
+    'target_record_key' => NULL,
+    'target_module' => NULL,
+    'width' => '10%',
+    'default' => true,
+  ),
   'invoice_date' => 
   array (
     'vname' => 'LBL_INVOICE_DATE',
@@ -61,5 +64,17 @@ $subpanel_layout['list_fields'] = array (
     'vname' => 'LBL_PRODUCT_TOTAL_PRICE',
     'width' => '10%',
     'default' => true,
+  ),
+  'agreement_name2' => 
+  array (
+    'type' => 'text',
+    'link' => true,
+    'vname' => 'Agreement Number',
+    'sortable' => false,
+    'width' => '10%',
+    'default' => true,
+    'widget_class' => 'SubPanelDetailViewLink',
+    'target_module' => 'CM3_Renewals',
+    'target_record_key' => 'renewal_id',
   ),
 );
