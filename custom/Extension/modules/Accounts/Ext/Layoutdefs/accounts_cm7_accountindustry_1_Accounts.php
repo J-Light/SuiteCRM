b7af4acd8d11b6dc8cc29aa7bfd2417f9ad3ee21
Nishit Joseph <nish.joseph@leapaust.com.au>
2018-07-10 11:21:10 +1000

    Hide Account industries

--- custom/Extension/modules/Accounts/Ext/Layoutdefs/accounts_cm7_accountindustry_1_Accounts.php
+++ custom/Extension/modules/Accounts/Ext/Layoutdefs/accounts_cm7_accountindustry_1_Accounts.php
@@ -1,23 +1,23 @@
 <?php
- // created: 2017-09-04 12:38:00
-$layout_defs["Accounts"]["subpanel_setup"]['accounts_cm7_accountindustry_1'] = array (
-  'order' => 100,
-  'module' => 'CM7_AccountIndustry',
-  'subpanel_name' => 'default',
-  'sort_order' => 'asc',
-  'sort_by' => 'id',
-  'title_key' => 'LBL_ACCOUNTS_CM7_ACCOUNTINDUSTRY_1_FROM_CM7_ACCOUNTINDUSTRY_TITLE',
-  'get_subpanel_data' => 'accounts_cm7_accountindustry_1',
-  'top_buttons' => 
-  array (
-    0 => 
-    array (
-      'widget_class' => 'SubPanelTopButtonQuickCreate',
-    ),
-    1 => 
-    array (
-      'widget_class' => 'SubPanelTopSelectButton',
-      'mode' => 'MultiSelect',
-    ),
-  ),
-);
+// created: 2017-09-04 12:38:00
+// $layout_defs["Accounts"]["subpanel_setup"]['accounts_cm7_accountindustry_1'] = array (
+//   'order' => 100,
+//   'module' => 'CM7_AccountIndustry',
+//   'subpanel_name' => 'default',
+//   'sort_order' => 'asc',
+//   'sort_by' => 'id',
+//   'title_key' => 'LBL_ACCOUNTS_CM7_ACCOUNTINDUSTRY_1_FROM_CM7_ACCOUNTINDUSTRY_TITLE',
+//   'get_subpanel_data' => 'accounts_cm7_accountindustry_1',
+//   'top_buttons' => 
+//   array (
+//     0 => 
+//     array (
+//       'widget_class' => 'SubPanelTopButtonQuickCreate',
+//     ),
+//     1 => 
+//     array (
+//       'widget_class' => 'SubPanelTopSelectButton',
+//       'mode' => 'MultiSelect',
+//     ),
+//   ),
+// );
