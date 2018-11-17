b611911a58c98d4198ef83040a893362a2e6ee89
Nishit Joseph <nish.joseph@leapaust.com.au>
2018-11-17 18:29:40 +1100

    Sort quotes and invoices for contacts

---
+++ custom/Extension/modules/Contacts/Ext/Layoutdefs/sort_quotes_invoices.php
@@ -0,0 +1,8 @@
+<?php
+
+$layout_defs['Contacts']['subpanel_setup']['account_aos_quotes']['sort_order'] = 'desc';
+$layout_defs['Contacts']['subpanel_setup']['account_aos_quotes']['sort_by'] = 'number';
+
+$layout_defs['Contacts']['subpanel_setup']['account_aos_invoices']['sort_order'] = 'desc';
+$layout_defs['Contacts']['subpanel_setup']['account_aos_invoices']['sort_by'] = 'number';
+
